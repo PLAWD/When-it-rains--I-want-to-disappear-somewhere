4e074818af1467cb268a0931f89fbf999ba4b99f
PLAWD <santosrohanjamest@gmail.com>
2023-11-13 23:15:43 +0800

    Updated Textures
    no errors na :)

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from settings import *
 from map import *
 from player import *
 from raycasting import *
+from object_renderer import *
+
 
 
 class Game:
@@ -17,6 +19,7 @@ class Game:
     def new_game(self):
         self.map = Map(self)
         self.player = Player(self)
+        self.object_renderer = ObjectRenderer(self)
         self.raycasting = RayCasting(self)
 
     def update(self):
@@ -28,6 +31,7 @@ class Game:
 
     def draw(self):
         self.screen.fill('black')
+        self.object_renderer.draw()
         # self.map.draw()
         # self.player.draw()
 
